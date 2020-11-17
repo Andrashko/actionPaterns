0aa8e2641dee39f38e3083fde72b8a269516d146
Yurii Andrashko <yurii.andrashko@uzhnu.edu.ua>
2020-11-17 19:22:22 +0200

    Cahin sc

--- cs/Program.cs
+++ cs/Program.cs
@@ -7,6 +7,16 @@ namespace cs
     {
         static void Main(string[] args)
         {     
+
+            var Chain = new LogHendler();
+            Chain.SetNext(new AuthorizeHendler()).SetNext(new ResponceHendler());
+            Console.WriteLine(Chain.Handle(new Request("admin", "admin")));
+            Console.WriteLine(Chain.Handle(new Request("Noname", "No")));
+        }
+
+
+        private static void EventTest()
+        {
             var subject = new EventSubject();
             var less5 = new EventLessFiveObserver();
             var even = new EventEvenObserver();
@@ -19,56 +29,62 @@ namespace cs
             subject.RandomState();
         }
 
-        private static void All(){
-            // var subject = new Subject();
-            // var lessFive = new LessFiveObserver();
-            // var even = new EvenObserver();
-            // subject.Attach(lessFive);
-            // subject.Attach(even);
-            // subject.RandomState();
-            // subject.RandomState();
-            // subject.RandomState();
-            // subject.RandomState();
-            // subject.RandomState();
-            // subject.Detach(even);
-            // subject.RandomState();
-            // subject.RandomState();
-            // subject.RandomState();
-            // subject.RandomState();
-            // subject.RandomState();
-                
-        //    Cashbox cashbox = new Cashbox();
-        //    List<Payment> strategies = new List<Payment>(2) {new CashPayment(), new CardPayment() };
+        private static void StateTest()
+        {
+            CashboxWithState cashbox = new CashboxWithState();
+            while (true) 
+            {
+                Console.WriteLine("Введіть суму до оплати або 0 для виходу");
+                double sum = Double.Parse(Console.ReadLine());
+                if (sum<=0) 
+                    break;
+                Console.WriteLine("Виберіть статрегію оплати:");
+                foreach(string state in cashbox.States)
+                    Console.WriteLine(state);
+                cashbox.State = Console.ReadLine();
+                if (cashbox.TakePayment(sum))
+                    Console.WriteLine("Платіж успішний");
+                else
+                    Console.WriteLine("Платіж не успішний"); 
+            }          
+        }
+        private static void ObserverTest(){
+            var subject = new Subject();
+            var lessFive = new LessFiveObserver();
+            var even = new EvenObserver();
+            subject.Attach(lessFive);
+            subject.Attach(even);
+            subject.RandomState();
+            subject.RandomState();
+            subject.RandomState();
+            subject.RandomState();
+            subject.RandomState();
+            subject.Detach(even);
+            subject.RandomState();
+            subject.RandomState();
+            subject.RandomState();
+            subject.RandomState();
+            subject.RandomState();
+        }
+        public static void StrategyTest()
+        {
+           Cashbox cashbox = new Cashbox();
+           List<Payment> strategies = new List<Payment>(2) {new CashPayment(), new CardPayment() };
                     
-        //    while (true) {
-        //         Console.WriteLine("Введіть суму до оплати або 0 для виходу");
-        //         double sum = Double.Parse(Console.ReadLine());
-        //         if (sum<=0) 
-        //             break;
-        //         Console.WriteLine("Виберіть статрегію оплати \n 1. Готівкою \n 2. Картою");
-        //         int strategyIndex = Int32.Parse( Console.ReadLine()) - 1;
-        //         cashbox.setPaymentStrategy(strategies[strategyIndex]);
-        //         if (cashbox.TakePayment(sum))
-        //             Console.WriteLine("Платіж успішний");
-        //         else
-        //             Console.WriteLine("Платіж не успішний");    
-        //    } 
-
-            // CashboxWithState cashbox = new CashboxWithState();
-            // while (true) {
-            //     Console.WriteLine("Введіть суму до оплати або 0 для виходу");
-            //     double sum = Double.Parse(Console.ReadLine());
-            //     if (sum<=0) 
-            //         break;
-            //     Console.WriteLine("Виберіть статрегію оплати:");
-            //     foreach(string state in cashbox.States)
-            //         Console.WriteLine(state);
-            //     cashbox.State = Console.ReadLine();
-            //     if (cashbox.TakePayment(sum))
-            //         Console.WriteLine("Платіж успішний");
-            //     else
-            //         Console.WriteLine("Платіж не успішний"); 
-            // }           
+           while (true)
+           {
+                Console.WriteLine("Введіть суму до оплати або 0 для виходу");
+                double sum = Double.Parse(Console.ReadLine());
+                if (sum<=0) 
+                    break;
+                Console.WriteLine("Виберіть статрегію оплати \n 1. Готівкою \n 2. Картою");
+                int strategyIndex = Int32.Parse( Console.ReadLine()) - 1;
+                cashbox.setPaymentStrategy(strategies[strategyIndex]);
+                if (cashbox.TakePayment(sum))
+                    Console.WriteLine("Платіж успішний");
+                else
+                    Console.WriteLine("Платіж не успішний");    
+           } 
 
         }
     }
